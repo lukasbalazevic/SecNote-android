4677e1e3766f21ccffa43165675693f7844531dc
lukas.balazevic <lukas.balazevic@gmail.com>
2020-03-13 21:45:29 +0100

    Add dynamic start fragment to navigation

---
+++ app/src/main/kotlin/app/vut/secnote/data/ui/StartDestination.kt
@@ -0,0 +1,5 @@
+package app.vut.secnote.data.ui
+
+enum class StartDestination {
+    LOGIN, PIN, NOTES
+}
--- app/src/main/kotlin/app/vut/secnote/ui/navigation/NavigationActivity.kt
+++ app/src/main/kotlin/app/vut/secnote/ui/navigation/NavigationActivity.kt
@@ -2,7 +2,11 @@ package app.vut.secnote.ui.navigation
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.navigation.fragment.NavHostFragment
 import app.vut.secnote.R
+import app.vut.secnote.data.ui.StartDestination
 import app.vut.secnote.databinding.ActivityNavigationBinding
 import app.vut.secnote.ui.base.BaseBindingActivity
 import javax.inject.Inject
@@ -19,4 +23,22 @@ class NavigationActivity :
     override lateinit var viewModelFactory: NavigationViewModelFactory
 
     override val layoutResId = R.layout.activity_navigation
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setupNavigationFragment(StartDestination.LOGIN)
+    }
+
+    private fun setupNavigationFragment(destination: StartDestination) {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment)?.navController?.apply {
+            val graph = navInflater.inflate(R.navigation.nav_graph).apply {
+                startDestination = when (destination) {
+                    StartDestination.LOGIN -> R.id.fragment_login
+                    StartDestination.PIN -> R.id.fragment_pin
+                    StartDestination.NOTES -> R.id.fragment_notes
+                }
+            }
+            setGraph(graph, intent.extras)
+        } ?: error("Unexpected state")
+    }
 }
