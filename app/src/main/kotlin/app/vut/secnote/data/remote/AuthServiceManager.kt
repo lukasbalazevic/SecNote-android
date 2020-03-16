98b63e7bd2b76b3ad9fded94a69585fdf2bad8ee
lukas.balazevic <lukas.balazevic@gmail.com>
2020-03-16 22:06:45 +0100

    Fix lints

--- app/src/main/kotlin/app/vut/secnote/data/remote/AuthServiceManager.kt
+++ app/src/main/kotlin/app/vut/secnote/data/remote/AuthServiceManager.kt
@@ -3,15 +3,10 @@ package app.vut.secnote.data.remote
 import app.vut.secnote.authservice.AuthServiceCoroutineGrpc
 import app.vut.secnote.domain.security.CryptoHelper
 import com.github.marcoferrer.krotoplus.coroutines.withCoroutineContext
-import io.grpc.CallOptions
-import io.grpc.Metadata
-import io.grpc.stub.MetadataUtils
 import javax.inject.Inject
 
-
 class AuthServiceManager @Inject constructor(
-    private val client: AuthServiceCoroutineGrpc.AuthServiceCoroutineStub,
-    private val cryptoHelper: CryptoHelper
+    private val client: AuthServiceCoroutineGrpc.AuthServiceCoroutineStub
 ) : ServiceManager() {
 
     suspend fun signIn(email: String, password: String, key: String) = executeApiCall {
@@ -28,21 +23,5 @@ class AuthServiceManager @Inject constructor(
             this.password = password
             this.key = key
         }
-    }
-
-    suspend fun executeWithMetadata(token: String, hash: String) = executeApiCall {
-        val message = "Request in string"
-        val signature = cryptoHelper.signAndEncodeDataBase64(message.toByteArray())
-        val encodedMessage = cryptoHelper.encodeBase64(message.toByteArray())
-
-        val header = Metadata()
-        header.put(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER), token)
-        header.put(Metadata.Key.of("Digest", Metadata.ASCII_STRING_MARSHALLER), encodedMessage)
-        header.put(Metadata.Key.of("Signature", Metadata.ASCII_STRING_MARSHALLER), signature)
-
-        MetadataUtils.attachHeaders(
-            client.withCoroutineContext(),
-            header
-        ).verify()
     }
 }
